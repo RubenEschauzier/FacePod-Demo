c0b019babf669950d7517b7141377833fb4a0342
RubenEschauzier <ruben.eschauzier@gmail.com>
2026-01-16 08:57:59 +0100

    Fix hardcoded baseURL

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Link-Traversal-Demo-SolidLab/',
+  base: '/FacePod-Demo/',
   server: {
     watch: {
       usePolling: true
